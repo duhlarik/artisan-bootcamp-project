a4fb8d9ab6d6d3d19363f8756ce5656eb79aa04a
hoff2 <choffman@pillartechnology.com>
2018-11-16 15:12:52 -0600

    controller and model for fake bank service

--- fake-bank-service/src/main/java/com/pillar/fakeBankService/FakeBankServiceController.java
+++ fake-bank-service/src/main/java/com/pillar/fakeBankService/FakeBankServiceController.java
@@ -1,4 +1,16 @@
 package com.pillar.fakeBankService;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FakeBankServiceController {
+
+    @PostMapping("/transaction")
+    public ResponseEntity<?> postTransaction(@RequestBody TransactionRequest transaction) {
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
--- fake-bank-service/src/main/java/com/pillar/fakeBankService/TransactionRequest.java
+++ fake-bank-service/src/main/java/com/pillar/fakeBankService/TransactionRequest.java
@@ -1,4 +1,34 @@
 package com.pillar.fakeBankService;
 
+import java.util.Date;
+
 public class TransactionRequest {
+
+    public TransactionRequest() {}
+
+    private String cardNumber;
+    private Double amount;
+    private Double creditLimit;
+    private Date dateOfTransaction;
+    private String customer;
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public Double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public Date getDateOfTransaction() {
+        return dateOfTransaction;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
 }

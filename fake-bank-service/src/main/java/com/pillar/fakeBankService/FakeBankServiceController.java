d55cded24b852ca482f1c98188237543b40c6bfa
hoff2 <choffman@pillartechnology.com>
2018-11-16 15:27:47 -0600

    add balance/credit limit check to fake bank service

--- fake-bank-service/src/main/java/com/pillar/fakeBankService/FakeBankServiceController.java
+++ fake-bank-service/src/main/java/com/pillar/fakeBankService/FakeBankServiceController.java
@@ -9,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FakeBankServiceController {
 
+    private Double currentBalance = 7000.0;
+
     @PostMapping("/transaction")
     public ResponseEntity<?> postTransaction(@RequestBody TransactionRequest transaction) {
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        if (transaction.getAmount() + currentBalance <= transaction.getCreditLimit()) {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
     }
 }

8a2c47321d89ffb998b1043a303df6616ede07fc
Daman Singh <dsingh@pillartechnology.com>
2018-11-19 14:06:02 -0600

    added Transaction POJO and along with its unit tests

--- src/main/java/com/pillar/transaction/Transaction.java
+++ src/main/java/com/pillar/transaction/Transaction.java
@@ -1,4 +1,52 @@
 package com.pillar.transaction;
 
+import java.util.Date;
+
 public class Transaction {
+
+    private String cardNumber;
+    private Double amount;
+    private Date dateOfTransaction;
+    private Integer customerId;
+
+    public Transaction() {}
+
+    public Transaction(String cardNumber, Double amount, Date dateOfTransaction, Integer customerId) {
+        this.cardNumber = cardNumber;
+        this.amount = amount;
+        this.dateOfTransaction = dateOfTransaction;
+        this.customerId = customerId;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDateOfTransaction() {
+        return dateOfTransaction;
+    }
+
+    public void setDateOfTransaction(Date dateOfTransaction) {
+        this.dateOfTransaction = dateOfTransaction;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
 }

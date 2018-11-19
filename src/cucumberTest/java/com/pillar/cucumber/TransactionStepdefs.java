fd45dd2c2b3f7150131252bedf711a3fb49a423d
Andrew Kirkegaard <akirkegaard@pillartechnology.com>
2018-11-19 15:45:05 -0600

    Refactor Transaction acceptance test to use Account API directly

--- src/cucumberTest/java/com/pillar/cucumber/TransactionStepdefs.java
+++ src/cucumberTest/java/com/pillar/cucumber/TransactionStepdefs.java
@@ -1,6 +1,5 @@
 package com.pillar.cucumber;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import com.pillar.account.Account;
 
 import cucumber.api.java.en.Given;
@@ -9,56 +8,57 @@ import cucumber.api.java.en.When;
 
 import java.util.Date;
 import java.util.HashMap;
-import  java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import static java.util.UUID.randomUUID;
 import static org.junit.Assert.assertEquals;
 
 public class TransactionStepdefs {
-    private String dbUrl;
-    private String endpoint;
-    private String fakeServiceUrl;
-    private ClientResponse response;
     private final WebClient client;
     private final WebClient fakeServiceClient;
+
     private Account account;
-    private UUID activeCreditCardNumber;
-
-
+    private ClientResponse response;
 
     public TransactionStepdefs() {
-        dbUrl = System.getProperty("integration-mysql", "jdbc:mysql://localhost:3316/cc_processing");
-        endpoint = System.getProperty("integration-endpoint", "http://localhost:8080");
-        fakeServiceUrl = System.getProperty("fake-service-base-url", "http://localhost:5000");
+        String endpoint = System.getProperty("integration-endpoint", "http://localhost:8080");
+        String fakeServiceUrl = System.getProperty("fake-service-base-url", "http://localhost:5000");
         client = WebClient.create(endpoint);
         fakeServiceClient = WebClient.create(fakeServiceUrl);
-        activeCreditCardNumber = randomUUID();
     }
 
     @Given("an active card and account")
     public void anActiveCardAndAccount(){
-        createAccount();
-        String query = "SELECT * FROM account WHERE credit_card_number = ?";
-        account = (Account)getJdbcTemplate().queryForObject(
-                query, new Object[] { activeCreditCardNumber.toString() }, new AccountRowMapper());
+        final HashMap<String, String> payload = new HashMap<>();
+        payload.put("cardholderName", "Steve Goliath");
+        payload.put("ssn", "123-45-6788");
+        payload.put("businessName", "Target");
+
+        final ClientResponse response = client
+                .post()
+                .uri("/api/account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(payload))
+                .exchange()
+                .block();
+
+        account = response.bodyToMono(Account.class).block();
     }
 
     @When("a purchase transaction request is made,")
     public void aPurchaseTransactionRequestIsMade() {
         HashMap<String, Object> transaction = new HashMap<>();
-        transaction.put("cardNumber", activeCreditCardNumber.toString());
+        transaction.put("cardNumber", account.getCardNumber());
         transaction.put("amount", 2.00);
-        transaction.put("creditLimit", 10000);
+        transaction.put("creditLimit", account.getCreditLimit());
         transaction.put("dateOfTransaction", new Date());
-        transaction.put("customer", "Foobar");
-        response =  fakeServiceClient
+        transaction.put("customerId", 3);
+
+        response = fakeServiceClient
                 .post()
                 .uri("/api/transaction")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -70,18 +70,5 @@ public class TransactionStepdefs {
     @Then("success response is returned")
     public void successResponseIsReturned() {
         assertEquals(HttpStatus.CREATED, response.statusCode());
-    }
-
-    private JdbcTemplate getJdbcTemplate() {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setURL(dbUrl);
-        dataSource.setUser("root");
-        dataSource.setPassword("password");
-        return new JdbcTemplate(dataSource);
-    }
-
-    private void createAccount() {
-        JdbcTemplate template = getJdbcTemplate();
-        template.update("INSERT INTO account SET credit_card_number = ?, active = ?", activeCreditCardNumber.toString(), 1);
     }
 }

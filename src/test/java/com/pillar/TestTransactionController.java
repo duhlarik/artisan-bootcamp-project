57c3cd84557ec8254777121af7f37cc25b18ee73
Oge Nnadi <onnadi@pillartechnology.com>
2019-04-18 20:14:43 -0500

    Refactor

--- src/test/java/com/pillar/TestTransactionController.java
+++ src/test/java/com/pillar/TestTransactionController.java
@@ -4,50 +4,39 @@ import com.pillar.account.Account;
 import com.pillar.account.AccountRepository;
 import com.pillar.cardholder.Cardholder;
 import com.pillar.transaction.TransactionBankRequest;
-import com.pillar.transaction.TransactionRecord;
 import com.pillar.transaction.TransactionRecordRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Date;
 
-import static com.pillar.TransactionController.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class TestTransactionController {
 
-    public static final int CREDIT_LIMIT = 10000;
-    public static final String CARD_NUMBER = "1234";
-    public static final String JUNK_RETAILER = "JUNK RETAILER";
-    public static final double SMALL_AMOUNT = 1.0;
-    public static final boolean APPROVED = true;
-    public static final Instant NOW = Instant.now();
+    private static final int CREDIT_LIMIT = 10000;
+    private static final String CARD_NUMBER = "1234";
+    private static final boolean APPROVED = true;
     private TransactionController controller;
     private BankService bankService;
-    private AccountRepository accountRepository;
-    private Account testAccount;
-    private TransactionRecordRepository transactionRecordRepository;
 
     private TransactionBankRequest validTransactionBankRequest;
     private TransactionBankRequest invalidTransactionBankRequest;
 
     @Before
     public void setUp() {
-        accountRepository = mock(AccountRepository.class);
-        transactionRecordRepository = mock(TransactionRecordRepository.class);
+        AccountRepository accountRepository = mock(AccountRepository.class);
+        TransactionRecordRepository transactionRecordRepository = mock(TransactionRecordRepository.class);
         bankService = mock(BankService.class);
 
         controller = new TransactionController(accountRepository, transactionRecordRepository, bankService);
 
-        testAccount = new Account(1, CREDIT_LIMIT, CARD_NUMBER, APPROVED, new Cardholder());
+        Account testAccount = new Account(1, CREDIT_LIMIT, CARD_NUMBER, APPROVED, new Cardholder());
 
         when(accountRepository.findByCardNumber(CARD_NUMBER)).thenReturn(testAccount);
 
@@ -80,46 +69,5 @@ public class TestTransactionController {
 
     private void postInvalidTransaction() {
         when(bankService.postTransaction(invalidTransactionBankRequest)).thenReturn(HttpStatus.FORBIDDEN);
-    }
-
-    @Test
-    public void returns403IfRequestAmountAndBalanceGreaterThanCreditLimit() {
-        double balance = CREDIT_LIMIT;
-        Double amount = SMALL_AMOUNT;
-        ArrayList<TransactionRecord> transactionRecordList = new ArrayList<>();
-        transactionRecordList.add(new TransactionRecord(balance, NOW, APPROVED, testAccount));
-        TransactionRequest request = new TransactionRequest(CARD_NUMBER, amount, NOW, JUNK_RETAILER);
-        when(transactionRecordRepository.save(any())).thenReturn(new TransactionRecord());
-        when(transactionRecordRepository.findAllByAccount(testAccount)).thenReturn(transactionRecordList);
-
-        ResponseEntity<?> transactionResponse = controller.createDbTransaction(request);
-
-        assertEquals(HttpStatus.FORBIDDEN, transactionResponse.getStatusCode());
-    }
-
-    @Test
-    public void returns201CreatedWhenAmountLessThanCreditLimit() {
-        Double amountLessThanLimit = CREDIT_LIMIT - SMALL_AMOUNT;
-        TransactionRequest request = new TransactionRequest(CARD_NUMBER, amountLessThanLimit, NOW, JUNK_RETAILER);
-        when(transactionRecordRepository.save(any())).thenReturn(new TransactionRecord());
-
-        ResponseEntity<?> transactionResponse = controller.createDbTransaction(request);
-
-        assertEquals(HttpStatus.CREATED, transactionResponse.getStatusCode());
-    }
-
-    @Test
-    public void returns201CreatedIfBalancePlusTransactionAmountEqualsCreditLimit() {
-        Double balance = CREDIT_LIMIT - SMALL_AMOUNT;
-        Double amount  = SMALL_AMOUNT;
-        ArrayList<TransactionRecord> transactionRecordList = new ArrayList<>();
-        transactionRecordList.add(new TransactionRecord(balance, NOW, APPROVED, testAccount));
-        TransactionRequest request = new TransactionRequest(CARD_NUMBER, amount, NOW, JUNK_RETAILER);
-        when(transactionRecordRepository.save(any())).thenReturn(new TransactionRecord());
-        when(transactionRecordRepository.findAllByAccount(testAccount)).thenReturn(transactionRecordList);
-
-        ResponseEntity<?> transactionResponse = controller.createDbTransaction(request);
-
-        assertEquals(HttpStatus.CREATED, transactionResponse.getStatusCode());
     }
 }

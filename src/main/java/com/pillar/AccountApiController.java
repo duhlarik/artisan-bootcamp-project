767cbba02411ab330970480b440ccfcadddee38e
Oge Nnadi <onnadi@pillartechnology.com>
2019-04-22 14:13:23 -0500

    Refactor

--- src/main/java/com/pillar/AccountApiController.java
+++ src/main/java/com/pillar/AccountApiController.java
@@ -89,8 +89,9 @@ public class AccountApiController {
 
     @GetMapping(path = "/{cardNumber}/rewards/{retailer}")
     public ResponseEntity<Double> getRewardsBalance(String cardNumber, String retailer) {
-        if(rewardsProgrammeRepository.existsByRetailer(retailer)){
-            Account account = accountRepository.findOneByCreditCardNumber(cardNumber).get();
+        final Optional<Account> found = accountRepository.findOneByCreditCardNumber(cardNumber);
+        if(found.isPresent() && rewardsProgrammeRepository.existsByRetailer(retailer)){
+            Account account = found.get();
             ArrayList<TransactionRecord> transactionRecordList = this.transactionRecordRepository.findAllByAccount(account);
             double chargeBalance = TransactionRecordGenerator.calculateChargeBalance(transactionRecordList);
             double percentage = rewardsProgrammeRepository.findOneByRetailer(retailer).get().getPercentage();
